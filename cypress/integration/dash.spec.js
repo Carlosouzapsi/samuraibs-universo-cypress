e7da1dfaee08e7cad26c2550b4a251f32b2ea84a
carlosouzapsi <carlosouzapsi@gmail.com>
2022-05-20 14:24:14 -0300

    atualizando aulas

--- cypress/integration/dash.spec.js
+++ cypress/integration/dash.spec.js
@@ -1,43 +1,33 @@
-import loginPage from "../support/pages/login";
+
 import dashPage  from "../support/pages/dash";
+
+import {costumer, provider, appointment} from "../support/factories/dash"
 
 describe("dashboard", function () {
   context("quando o cliente faz um agendamento no app mobile", function () {
-    const data = {
-      costumer: {
-        name: "Nikk Sixx",
-        email: "sixx@motleycrue.com",
-        password: "pwd123",
-        is_provider: false,
-      },
-      provider: {
-        name: "Ramon Valdes",
-        email: "ramon@televisa.com",
-        password: "pwd123",
-        is_provider: true,
-      },
-      appointmentHour: '14:00'
-    };
+
 
     before(function () {
-      cy.postUser(data.provider);
-      cy.postUser(data.costumer);
+      cy.postUser(provider);
+      cy.postUser(costumer);
 
-      cy.apiLogin(data.costumer);
-      cy.setProviderId(data.provider.email);
-      cy.createAppointment(data.appointmentHour);
+      cy.apiLogin(costumer);
+      cy.setProviderId(provider.email);
+      cy.createAppointment(appointment.hour);
     });
 
     it("o mesmo deve ser exibido no dashboard", function () {
-      loginPage.go();
-      loginPage.form(data.provider);
-      loginPage.submit();
-
-      dashPage.calendarShouldBeVisible();
       const day = Cypress.env('appointmentDay')
-      dashPage.selectDay(day);
+      // loginPage.go();
+      // loginPage.form(provider);
+      // loginPage.submit();
 
-      dashPage.appointmentShouldBe(data.costumer, data.appointmentHour);
+      //cy.uiLogin(provider)
+      cy.apiLogin(provider, true);
+
+      dashPage.calendarShouldBeVisible();      
+      dashPage.selectDay(day);
+      dashPage.appointmentShouldBe(costumer, appointment.hour);
 
     });
   });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,14 +23,31 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-import moment from "moment";
+import moment from "moment"
+
+import { apiServer } from '../../cypress.json'
+
+import loginPage from './pages/login'
+import dashPage from './pages/dash'
+
+Cypress.Commands.add('uiLogin', function(user){
+  loginPage.go()
+  loginPage.form(user)
+  loginPage.submit()
+  dashPage.header.userLoggedIn(user.name)
+
+})
 
 Cypress.Commands.add("postUser", function (user) {
   cy.task("removeUser", user.email).then(function (result) {
     console.log(result);
   });
 
-  cy.request("POST", "http://localhost:3333/users", user).then(function (
+  cy.request({
+    method: "POST", 
+    url: apiServer + "/users", 
+    body: user
+  }).then(function (
     response
   ) {
     expect(response.status).to.eq(200);
@@ -38,8 +55,10 @@ Cypress.Commands.add("postUser", function (user) {
 });
 
 Cypress.Commands.add("recoveryPass", function (email) {
-  cy.request("POST", "http://localhost:3333/password/forgot", {
-    email: email,
+  cy.request({
+    method: "POST", 
+    url: apiServer + "/password/forgot", 
+    body:{ email: email },
   }).then(function (response) {
     expect(response.status).to.eq(204);
 
@@ -55,7 +74,7 @@ Cypress.Commands.add("recoveryPass", function (email) {
 
     Cypress.env("appointmentDay", now.getDate());
 
-    const date = moment(now).format("YYYY-MM-DD " + hour + ":00");
+    const date = moment(now).format(`YYYY-MM-DD ${hour}:00`);
 
     const payload = {
       provider_id: Cypress.env("providerId"),
@@ -90,7 +109,7 @@ Cypress.Commands.add("createAppointment", function (hour) {
 
   cy.request({
     method: "POST",
-    url: "http://localhost:3333/appointments",
+    url: apiServer + "/appointments",
     body: payload,
     headers: {
       authorization: "Bearer " + Cypress.env("apiToken"),
@@ -103,7 +122,7 @@ Cypress.Commands.add("createAppointment", function (hour) {
 Cypress.Commands.add("setProviderId", function (providerEmail) {
   cy.request({
     method: "GET",
-    url: "http://localhost:3333/providers",
+    url: apiServer + "/providers",
     headers: {
       authorization: "Bearer " + Cypress.env("apiToken"),
     },
@@ -120,18 +139,30 @@ Cypress.Commands.add("setProviderId", function (providerEmail) {
   });
 });
 
-Cypress.Commands.add("apiLogin", function (user) {
+Cypress.Commands.add("apiLogin", function (user, setLocalStorage = false) {
   const payload = {
     email: user.email,
-    password: user.password,
+    password: user.password
   };
 
   cy.request({
     method: "POST",
-    url: "http://localhost:3333/sessions",
+    url: apiServer  + "/sessions",
     body: payload,
   }).then(function (response) {
     expect(response.status).to.eq(200);
     Cypress.env("apiToken", response.body.token);
-  });
-});
+
+    if(setLocalStorage){
+      const { token, user } = response.body;
+
+      window.localStorage.setItem('@Samurai:token', token);
+      window.localStorage.setItem('@Samurai:user', JSON.stringify(user));
+    }
+
+  })
+
+  if(setLocalStorage){
+    cy.visit('/dashboard')
+  }
+})
